9a7e8ecbf3be48f675692be50d1fe214ec93d439
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-08-24 22:21:28 +0530

    Time: 4 ms (67.1%), Space: 94.3 MB (67.56%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,4 +1,14 @@
 class Solution {
+    public ListNode Reverse(ListNode head,ListNode slow){
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+        return prev;
+    }
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
 
@@ -7,22 +17,13 @@ class Solution {
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        ListNode prev = null;
-        while (slow != null) {
-            ListNode next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
         ListNode left = head;
-        ListNode right = prev;
+        ListNode right = Reverse(head,slow);
         while (right != null) {
             if (left.val != right.val) return false;
             left = left.next;
             right = right.next;
         }
-
         return true;
     }
 }
