08f7cfb835d41668349c570e5ca859554c888064
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-08-31 23:22:53 +0530

    Time: 4 ms (99.87%), Space: 105.8 MB (96.69%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ListNode prev = head;
+        ListNode curr = head.next;
+        int index = 1;
+        int first = -1;
+        int last = -1;
+        int minDist = Integer.MAX_VALUE;
+        while (curr.next != null) {
+            if ((curr.val > prev.val && curr.val > curr.next.val) ||
+                (curr.val < prev.val && curr.val < curr.next.val)) {
+                if (first == -1) {
+                    first = index;
+                } else {
+                    minDist = Math.min(minDist, index - last);
+                }
+                last = index;
+            }
+            prev = curr;
+            curr = curr.next;
+            index++;
+        }
+        if (first == last) {
+            return new int[]{-1, -1};
+        }
+        int maxDist = last - first;
+        return new int[]{minDist, maxDist};
+    }
+}
