971ae20e3b1879c3367f3455383b26fc40602995
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-09-05 18:33:57 +0530

    Time: 4 ms (88.55%), Space: 126.4 MB (91.98%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -3,27 +3,19 @@ class Solution {
         int n = nums.length;
         int[] Min = new int[n];
         int[] Max = new int[n];
-
         int max = nums[0];
-
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, nums[i]);
-            Max[i] = max;
-        }
-
         int min = nums[n - 1];
-
         for (int j = n - 1; j >= 0; j--) {
             min = Math.min(min, nums[j]);
             Min[j] = min;
         }
-
-        for (int j = 0; j < n; j++) {
-            if (Max[j] - Min[j] <= k) {
-                return j;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            Max[i] = max;
+            if (Max[i] - Min[i] <= k) {
+                return i;
             }
         }
-
         return -1;
     }
 }
