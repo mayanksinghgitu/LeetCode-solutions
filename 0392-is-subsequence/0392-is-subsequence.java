2d5f0b59d8254057ddd1d078ba64ac3e2b9186ca
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-09-06 22:28:09 +0530

    Time: 2 ms (73.18%), Space: 42.9 MB (27.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -5,15 +5,12 @@ class Solution {
             char c=s.charAt(i);
             stack.push(c);
         }
-        int count=0;
         if(stack.isEmpty()) return stack.isEmpty();
         for(int i=0;i<t.length();i++){
             if(!stack.isEmpty() && stack.peek()==t.charAt(i)){
-                count++;
                 stack.pop();
             }
         }
-        if(count==s.length()) return stack.isEmpty();
         return stack.isEmpty();
         
     }
