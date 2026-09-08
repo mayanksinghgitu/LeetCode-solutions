4b3c18a1024f5d33acfc7f71c65056bf9cacc54f
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-09-08 12:15:19 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,7 +1,7 @@
 class Solution {
     public int countCommas(int n) {
-        String str=n+"";
+        String str = Integer.toString(n);
         if(str.length()<4) return 0;
-        return n-1000+1;
+        return n-999;
     }
 }
