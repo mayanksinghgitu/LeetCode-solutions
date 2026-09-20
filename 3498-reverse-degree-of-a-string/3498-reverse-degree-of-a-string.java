8446cee3ba811a549cddd0dd5c45a2ebdde8ff9f
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-09-20 13:39:18 +0530

    Time: 1 ms (100%), Space: 43.8 MB (81.16%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,17 +1,9 @@
 class Solution {
     public int reverseDegree(String s) {
-        String alp="abcdefghijklmnopqrstuvwxyz";
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(int i=1;i<=26;i++){
-            map.put(alp.charAt(26-i),i); 
-        }
-        int ans=0;
-        int idx=1;
-        for(char c : s.toCharArray()){
-            if(map.containsKey(c)){
-                ans+=idx*map.get(c);
-                idx++;
-            } 
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = 'z' - s.charAt(i) + 1;
+            ans += value * (i + 1);
         }
         return ans;
     }
