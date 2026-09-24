52b48a13deafbd86fbf6a36caa9d290f2fe792a3
MAYANK SINGH <mayanksinghsara80@gmail.com>
2026-09-24 18:57:13 +0530

    Time: 18 ms (60.83%), Space: 47.4 MB (81.07%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,29 +1,25 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int n : nums){
-            if(n%2==0){
-                map.put(n,map.getOrDefault(n,0)+1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            if (n % 2 == 0) {
+                map.put(n, map.getOrDefault(n, 0) + 1);
             }
         }
         int ans = -1;
         int maxFreq = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-
-            int num = entry.getKey();
-            int freq = entry.getValue();
-
-            if (num % 2 == 0) {
-
-                if (freq > maxFreq || 
-                    (freq == maxFreq && num < ans)) {
-
+        for (int num : map.keySet()) {
+            int freq = map.get(num);
+            if (freq > maxFreq) {
+                maxFreq = Math.max(maxFreq, freq);
+                ans = num;
+            } 
+            else if (freq == maxFreq) {
+                if (ans == -1 || num < ans) {
                     ans = num;
-                    maxFreq = freq;
                 }
             }
         }
-
         return ans;
     }
 }
